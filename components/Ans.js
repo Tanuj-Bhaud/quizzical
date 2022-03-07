084198b20f8209e8f75b00c78143c4e7b8c517c7
Tanuj Bhaud <91090701+Tanuj-Bhaud@users.noreply.github.com>
2022-03-07 20:45:06 +0530

    :construction:

--- components/Ans.js
+++ components/Ans.js
@@ -2,27 +2,30 @@ import { nanoid } from 'nanoid'
 import React, { useState } from 'react'
 
 export default function Ans(props) {
-  const [isSelected, setIsSelected] = useState(false)
-  const styles = {
-    backgroundColor: isSelected ? '#D6DBF5' : 'white',
-  }
   const handleClick = () => {
-    setIsSelected(!isSelected)
-    console.log(isSelected)
+    //GEt the answerr of  this button, and set it to the source fo truth
+
+    props.setSelectedButtons([
+      { selected: props.answer },
+      { selected: props.answer },
+      { selected: props.answer },
+      { selected: props.answer },
+      { selected: props.answer },
+    ])
   }
 
-  const aid = props.id
+  // props.setSelectedButtons.map((item, index )=> {
+  //   {}
+  // })
 
   return (
-    <div>
-      <button
-        onClick={handleClick}
-        id={aid}
-        style={styles}
-        className="m-2 rounded-xl border border-[#4D5B9E] py-2 px-4 font-semibold text-[#293264]"
-      >
-        {props.answer}
-      </button>
-    </div>
+    <button
+      onClick={handleClick}
+      className={` ${
+        props.selectedButtons ? 'bg-[#D6DBF5]' : ''
+      } m-2 rounded-xl border border-[#4D5B9E] py-2 px-4 font-semibold text-[#293264]`}
+    >
+      {props.answer}
+    </button>
   )
 }
--- components/Question.js
+++ components/Question.js
@@ -1,53 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { nanoid } from 'nanoid'
 import Ques from './Ques'
 import Ans from './Ans'
 import CheckButton from './CheckButton'
 
 export default function Question() {
   const [questions, setQuestions] = useState([])
-  // const [list, setList] = useState(allNewQuestions())
-  let q
-  const style = {
-    // backgroundColor: props.isHeld ? '#D6DBF5' : 'white',
-  }
-
+  const [selectedButtons, setSelectedButtons] = useState([])
+  useEffect(() => {
+    console.log({ selectedButtons })
+  }, [selectedButtons])
   useEffect(() => {
     axios
       .get('https://api.trivia.willfry.co.uk/questions')
       .then((res) => {
         console.log(res.data)
-        setQuestions(res.data)
+        const qData = res.data
+        qData.map((q) => {
+          // This is just setting the default state, doing it here instead of at the top cause the number of questions could change
+          setSelectedButtons((prev) => [...prev, { selected: '', key: '' }])
+
+          // Add incorrectAnswers and correctAnswer to the question object as answers and shuffle them so answer isn't always at the last
+          q.answers = [...q.incorrectAnswers, q.correctAnswer].sort(
+            () => Math.random() - 0.5
+          )
+        })
+
+        setQuestions(qData)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
-  function setid() {
-    return nanoid()
-  }
-
   return (
     <div className="flex flex-col items-center justify-center">
-      {questions.map((question) => {
+      {questions.map((question, index) => {
         return (
-          <div className="">
-            <Ques question={question.question} />
+          <div className="" key={index}>
+            <Ques question={question.question + ` ${question.correctAnswer}`} />
 
             <div className="mt-2 mb-4 flex flex-row justify-center">
-              {question.incorrectAnswers.map((answer) => {
+              {question.answers.map((answer, index) => {
                 return (
-                  <div>
-                    <Ans answer={answer} id={setid()} />
-                  </div>
+                  <Ans
+                    answer={answer}
+                    correct={question.correctAnswer}
+                    key={index}
+                    selectedButtons={selectedButtons}
+                    setSelectedButtons={setSelectedButtons}
+                  />
                 )
               })}
 
-              <button className="m-2 rounded-xl border border-[#4D5B9E] py-2 px-4 font-semibold text-[#293264]">
+              {/* <button className="m-2 rounded-xl border border-[#4D5B9E] py-2 px-4 font-semibold text-[#293264]">
                 {question.correctAnswer}
-              </button>
+              </button> */}
             </div>
           </div>
         )
